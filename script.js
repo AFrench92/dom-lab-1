221dee17359dcdcdfbc33423f3b7850bcc36c5cd
tony <tonyfrenchdm@gmail.com>
2020-10-26 17:05:47 -0400

    finished lab

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ moneyContainer.addEventListener("submit", (e) => {
   let formData = new FormData(moneyContainer);
   let coin = formData.get("coin");
   let number = formData.get("amount");
-  console.log(number);
   for (let i = 1; i <= number; i++) {
     let newDiv = document.createElement("div");
     newDiv.setAttribute("class", "coin-shape");
@@ -32,4 +31,40 @@ moneyContainer.addEventListener("submit", (e) => {
     moneyContainer.append(newDiv);
   }
   moneyContainer.reset();
+});
+
+let lightBulb = document.querySelector(".light-bulb");
+console.dir(lightBulb);
+
+let onButton = document.querySelector(".on");
+const bulbOn = () => {
+  lightBulb.classList.add("light");
+};
+onButton.addEventListener("click", bulbOn);
+
+let offButton = document.querySelector(".off");
+
+const bulbOff = () => {
+  lightBulb.classList.remove("light");
+};
+offButton.addEventListener("click", bulbOff);
+
+let toggle = document.querySelector(".toggle");
+const toggleBulb = () => {
+  lightBulb.classList.toggle("light");
+};
+
+toggle.addEventListener("click", toggleBulb);
+
+let end = document.querySelector(".end");
+
+end.addEventListener("click", () => {
+  lightBulb.remove();
+  onButton.removeEventListener("click", bulbOn);
+  onButton.disabled = true;
+  offButton.removeEventListener("click", bulbOff);
+  offButton.disabled = true;
+  toggle.removeEventListener("click", toggleBulb);
+  toggle.disabled = true;
+  end.disabled = true;
 });
